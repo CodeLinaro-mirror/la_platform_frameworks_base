b7841c8f8de404bce1877aeb536a9576ef0274f2
Julia Reynolds <juliacr@google.com>
2017-08-24 10:35:53 -0400

    Add more functionality to channel groups
    Description, blocking, links to/from the app
    
    Test: cts, runtest systemui-notification
    Bug: 63927402
    Change-Id: Icc8caf319651f9ac2d622fb54110270c89bdff61
    (cherry picked from commit 005c8b9098120526fa69f98bce3a5e1f79e3699b)

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1505,6 +1505,19 @@ public class NotificationManagerService extends SystemService {
         savePolicyFile();
     }
 
+    private void createNotificationChannelGroup(String pkg, int uid, NotificationChannelGroup group,
+            boolean fromApp, boolean fromListener) {
+        Preconditions.checkNotNull(group);
+        Preconditions.checkNotNull(pkg);
+        mRankingHelper.createNotificationChannelGroup(pkg, uid, group,
+                fromApp);
+        if (!fromListener) {
+            mListeners.notifyNotificationChannelGroupChanged(pkg,
+                    UserHandle.of(UserHandle.getCallingUserId()), group,
+                    NOTIFICATION_CHANNEL_OR_GROUP_ADDED);
+        }
+    }
+
     private ArrayList<ComponentName> getSuppressors() {
         ArrayList<ComponentName> names = new ArrayList<ComponentName>();
         for (int i = mListenersDisablingEffects.size() - 1; i >= 0; --i) {
@@ -1816,6 +1829,14 @@ public class NotificationManagerService extends SystemService {
         }
 
         @Override
+        public void updateNotificationChannelGroupForPackage(String pkg, int uid,
+                NotificationChannelGroup group) throws RemoteException {
+            enforceSystemOrSystemUI("Caller not system or systemui");
+            createNotificationChannelGroup(pkg, uid, group, false, false);
+            savePolicyFile();
+        }
+
+        @Override
         public void createNotificationChannelGroups(String pkg,
                 ParceledListSlice channelGroupList) throws RemoteException {
             checkCallerIsSystemOrSameApp(pkg);
@@ -1823,12 +1844,7 @@ public class NotificationManagerService extends SystemService {
             final int groupSize = groups.size();
             for (int i = 0; i < groupSize; i++) {
                 final NotificationChannelGroup group = groups.get(i);
-                Preconditions.checkNotNull(group, "group in list is null");
-                mRankingHelper.createNotificationChannelGroup(pkg, Binder.getCallingUid(), group,
-                        true /* fromTargetApp */);
-                mListeners.notifyNotificationChannelGroupChanged(pkg,
-                        UserHandle.of(UserHandle.getCallingUserId()), group,
-                        NOTIFICATION_CHANNEL_OR_GROUP_ADDED);
+                createNotificationChannelGroup(pkg, Binder.getCallingUid(), group, true, false);
             }
             savePolicyFile();
         }
@@ -1971,6 +1987,14 @@ public class NotificationManagerService extends SystemService {
                 String pkg, int uid, boolean includeDeleted) {
             checkCallerIsSystem();
             return mRankingHelper.getNotificationChannelGroups(pkg, uid, includeDeleted);
+        }
+
+        @Override
+        public NotificationChannelGroup getPopulatedNotificationChannelGroupForPackage(
+                String pkg, int uid, String groupId, boolean includeDeleted) {
+            enforceSystemOrSystemUI("getPopulatedNotificationChannelGroupForPackage");
+            return mRankingHelper.getNotificationChannelGroupWithChannels(
+                    pkg, uid, groupId, includeDeleted);
         }
 
         @Override
@@ -2997,6 +3021,17 @@ public class NotificationManagerService extends SystemService {
         }
 
         @Override
+        public void updateNotificationChannelGroupFromPrivilegedListener(
+                INotificationListener token, String pkg, UserHandle user,
+                NotificationChannelGroup group) throws RemoteException {
+            Preconditions.checkNotNull(user);
+            verifyPrivilegedListener(token, user);
+            createNotificationChannelGroup(
+                    pkg, getUidForPackageAndUser(pkg, user), group, false, true);
+            savePolicyFile();
+        }
+
+        @Override
         public void updateNotificationChannelFromPrivilegedListener(INotificationListener token,
                 String pkg, UserHandle user, NotificationChannel channel) throws RemoteException {
             Preconditions.checkNotNull(channel);
@@ -3716,9 +3751,10 @@ public class NotificationManagerService extends SystemService {
             usageStats.registerSuspendedByAdmin(r);
             return isPackageSuspended;
         }
-
         final boolean isBlocked =
-                mRankingHelper.getImportance(pkg, callingUid) == NotificationManager.IMPORTANCE_NONE
+                mRankingHelper.isGroupBlocked(pkg, callingUid, r.getChannel().getGroup())
+                || mRankingHelper.getImportance(pkg, callingUid)
+                        == NotificationManager.IMPORTANCE_NONE
                 || r.getChannel().getImportance() == NotificationManager.IMPORTANCE_NONE;
         if (isBlocked) {
             Slog.e(TAG, "Suppressing notification from package by user request.");
