36ba944abb654c23d77cfb06b14001622c483e04
Shaoqi,Li <quic_shaoli@quicinc.com>
2020-11-04 18:53:43 +0800

    SystemUI: Change the method of start dismissKeyguard.
    Change the method of starting DismissingKeyguard service
    from AsyncTask to handle in status bar.
    
    Change-Id: I435902b03280c5c5aa41036692c49b7e536b4052

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3026,7 +3026,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                             && mStatusBarKeyguardViewManager.isOccluded()) {
                         mStatusBarKeyguardViewManager.addAfterKeyguardGoneRunnable(runnable);
                     } else {
-                        AsyncTask.execute(runnable);
+                        mHandler.post(runnable);
                     }
                 }
                 if (dismissShade) {
