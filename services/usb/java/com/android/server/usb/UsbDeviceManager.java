9b694881d56fd60f62c4569038af75de6335d6ef
Sriharsha Allenki <quic_sallenki@quicinc.com>
2021-01-05 14:09:40 +0530

    Increase debounce time for DISCONNECT processing
    During composition switch, some host PCs are taking more
    time in processing the disconnect of previous functions and
    enumerating the new functions. This causes the delay in
    receiving the CONNECTED uevent which triggers the fallback
    to default composition. Prevent this by increasing the
    debounce time from 1s to 3s (which is the average time
    taken by these PCs to enumerate the new functions) for
    device mode state update, while keeping the debounce time
    for host mode as is at 1s.
    
    Bug: 176779207
    Test: Verified enumeration of device with the delay
    Change-Id: I7ff58a1a9755939ccb26dad61969902ec91f2225
    (cherry picked from commit 5a14ba686170b0475a546df66ad8c7e0c6ee6abc)

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -134,7 +134,10 @@ public class UsbDeviceManager {
     // Delay for debouncing USB disconnects.
     // We often get rapid connect/disconnect events when enabling USB functions,
     // which need debouncing.
-    private static final int UPDATE_DELAY = 1000;
+    private static final int DEVICE_STATE_UPDATE_DELAY = 3000;
+
+    // Delay for debouncing USB disconnects on Type-C ports in host mode
+    private static final int HOST_STATE_UPDATE_DELAY = 1000;
 
     // Timeout for entering USB request mode.
     // Request is cancelled if host does not configure device within 10 seconds.
@@ -526,7 +529,7 @@ public class UsbDeviceManager {
             msg.arg1 = connected;
             msg.arg2 = configured;
             // debounce disconnects to avoid problems bringing up USB tethering
-            sendMessageDelayed(msg, (connected == 0) ? UPDATE_DELAY : 0);
+            sendMessageDelayed(msg, (connected == 0) ? DEVICE_STATE_UPDATE_DELAY : 0);
         }
 
         public void updateHostState(UsbPort port, UsbPortStatus status) {
@@ -541,7 +544,7 @@ public class UsbDeviceManager {
             removeMessages(MSG_UPDATE_PORT_STATE);
             Message msg = obtainMessage(MSG_UPDATE_PORT_STATE, args);
             // debounce rapid transitions of connect/disconnect on type-c ports
-            sendMessageDelayed(msg, UPDATE_DELAY);
+            sendMessageDelayed(msg, HOST_STATE_UPDATE_DELAY);
         }
 
         private boolean waitForState(String state) {
