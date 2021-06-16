744d02bf43457bd391110b140344e7356208e398
Julia Reynolds <juliacr@google.com>
2021-06-16 14:00:23 -0400

    Store DND rule owners
    Test: uiservicestest, cts
    Bug: 189332346
    Bug: 235823407
    
    Change-Id: Ie546c15c25fcbd193b67cb997220f075691e9bab
    Merged-In: Ie546c15c25fcbd193b67cb997220f075691e9bab
    (cherry picked from commit 422cffbefa80c3b248458ef8ffe58d5fcf90ea3b)
    Merged-In: Ie546c15c25fcbd193b67cb997220f075691e9bab
    (cherry picked from commit a837890c39d5a2428381356decb88263d23dba03)

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4408,7 +4408,7 @@ public class NotificationManagerService extends SystemService {
         }
 
         @Override
-        public String addAutomaticZenRule(AutomaticZenRule automaticZenRule) {
+        public String addAutomaticZenRule(AutomaticZenRule automaticZenRule, String pkg) {
             Objects.requireNonNull(automaticZenRule, "automaticZenRule is null");
             Objects.requireNonNull(automaticZenRule.getName(), "Name is null");
             if (automaticZenRule.getOwner() == null
@@ -4417,6 +4417,7 @@ public class NotificationManagerService extends SystemService {
                         "Rule must have a conditionproviderservice and/or configuration activity");
             }
             Objects.requireNonNull(automaticZenRule.getConditionId(), "ConditionId is null");
+            checkCallerIsSameApp(pkg);
             if (automaticZenRule.getZenPolicy() != null
                     && automaticZenRule.getInterruptionFilter() != INTERRUPTION_FILTER_PRIORITY) {
                 throw new IllegalArgumentException("ZenPolicy is only applicable to "
@@ -4424,7 +4425,7 @@ public class NotificationManagerService extends SystemService {
             }
             enforcePolicyAccess(Binder.getCallingUid(), "addAutomaticZenRule");
 
-            return mZenModeHelper.addAutomaticZenRule(automaticZenRule,
+            return mZenModeHelper.addAutomaticZenRule(pkg, automaticZenRule,
                     "addAutomaticZenRule");
         }
 
