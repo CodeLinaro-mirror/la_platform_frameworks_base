b0a53d67c98e17787b5e6d6c9f3fbf45080d4db7
Amit Agrawal <quic_amag@quicinc.com>
2022-04-11 16:37:00 +0530

    display: check WFD broadcaster's permission
    Check WFD broadcaster's permission so that WFD broadcaster must hold
    QC_WFD permission to send an intent to display service.
    
    Change-Id: Ic33895916512984b03428289a2944792f81a229f
    CRs-Fixed: 3115304

--- services/core/java/com/android/server/wm/DisplayRotation.java
+++ services/core/java/com/android/server/wm/DisplayRotation.java
@@ -236,6 +236,13 @@ public class DisplayRotation {
                     "org.codeaurora.intent.action.WIFI_DISPLAY_VIDEO";
 
     /**
+     * Broadcast Permission for Wifi Display
+     */
+
+    private static final String WIFI_DISPLAY_PERMISSION =
+                    "com.qualcomm.permission.wfd.QC_WFD";
+
+    /**
      * Wifi Display specific variables
      */
     private boolean mWifiDisplayConnected = false;
@@ -308,7 +315,9 @@ public class DisplayRotation {
                                         false/* forceRelayout */);
                             }
                         }
-                    }, new IntentFilter(ACTION_WIFI_DISPLAY_VIDEO), null, UiThread.getHandler());
+                    }, new IntentFilter(ACTION_WIFI_DISPLAY_VIDEO),
+                        WIFI_DISPLAY_PERMISSION,
+                        UiThread.getHandler());
                 }
             };
             t.start();
