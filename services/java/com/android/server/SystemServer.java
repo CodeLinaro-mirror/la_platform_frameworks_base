acd2b621b01687f00356c81131dcac563ac4368a
Sahitya Puligadda <quic_spuligad@quicinc.com>
2023-04-22 15:09:42 +0530

    Revert Clipboard Service changes.
    Change-Id: I861c29d353f50a07c7d1fc21d876c99bf254b4f5

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2578,11 +2578,9 @@ public final class SystemServer implements Dumpable {
         }
 
         // NOTE: ClipboardService depends on ContentCapture and Autofill
-        if(!enable1GLowMem){
-            t.traceBegin("StartClipboardService");
-            mSystemServiceManager.startService(ClipboardService.class);
-            t.traceEnd();
-        }
+        t.traceBegin("StartClipboardService");
+        mSystemServiceManager.startService(ClipboardService.class);
+        t.traceEnd();
 
         t.traceBegin("AppServiceManager");
         mSystemServiceManager.startService(AppBindingService.Lifecycle.class);
