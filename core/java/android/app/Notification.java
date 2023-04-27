71ef042b7425b345f8b8f350ec4828527e9f70b4
Ioana Alexandru <aioana@google.com>
2023-04-27 14:55:28 +0000

    Verify URI permissions for notification shortcutIcon.
    Bug: 277593270
    Test: atest NotificationManagerServiceTest
    Change-Id: Iaf2a9a82f18e018e60e6cdc020da6ebf7267e8b1
    (cherry picked from commit 8378f404de96927b1051765e93ad242860f5442c)

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -2835,6 +2835,8 @@ public class Notification implements Parcelable
                     }
                 }
             }
+
+            visitIconUri(visitor, extras.getParcelable(EXTRA_CONVERSATION_ICON));
         }
 
         if (isStyle(CallStyle.class) & extras != null) {
