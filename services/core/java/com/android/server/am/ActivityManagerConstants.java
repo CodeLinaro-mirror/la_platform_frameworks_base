e970715727cdea0bdc8051dc4ee7885a99303bfb
Sahitya Puligadda <quic_spuligad@quicinc.com>
2023-06-30 21:15:18 +0530

    Reduce max cached apps
    Change-Id: Ie5ff0dac92f85a16557a6912ba27509da352aac1
    (cherry picked from commit e72070849e7a73076afb70765188b62f8a32e7b3)

--- services/core/java/com/android/server/am/ActivityManagerConstants.java
+++ services/core/java/com/android/server/am/ActivityManagerConstants.java
@@ -148,7 +148,7 @@ final class ActivityManagerConstants extends ContentObserver {
      */
     static final String KEY_NETWORK_ACCESS_TIMEOUT_MS = "network_access_timeout_ms";
 
-    private static int DEFAULT_MAX_CACHED_PROCESSES = 32;
+    private static int DEFAULT_MAX_CACHED_PROCESSES = 10;
     private static final long DEFAULT_FGSERVICE_MIN_SHOWN_TIME = 2*1000;
     private static final long DEFAULT_FGSERVICE_MIN_REPORT_TIME = 3*1000;
     private static final long DEFAULT_FGSERVICE_SCREEN_ON_BEFORE_TIME = 1*1000;
