3b9cc8c818b5d8078f28c7733819ad6223d956c2
kodanda rami reddy v <quic_kreddyv@quicinc.com>
2023-11-22 15:46:00 +0530

    Verify URI permissions for notification shortcutIcon.
    Bug: 277593270
    Test: atest NotificationManagerServiceTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:936b58b12851269b878b44cc8df790b3afe9c3f5)
    Merged-In: Iaf2a9a82f18e018e60e6cdc020da6ebf7267e8b1
    Change-Id: Iaf2a9a82f18e018e60e6cdc020da6ebf7267e8b1

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -2881,6 +2881,8 @@ public class Notification implements Parcelable
                     }
                 }
             }
+
+            visitIconUri(visitor, extras.getParcelable(EXTRA_CONVERSATION_ICON));
         }
 
         if (isStyle(CallStyle.class) & extras != null) {
