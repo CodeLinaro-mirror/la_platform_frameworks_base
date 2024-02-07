c9e29efed36b3a77f70fc2412383306e78dd1be8
sthatika <quic_sthatika@quicinc.com>
2024-02-07 14:30:52 +0530

    Fix for CVE-2021-39670
    Change-Id: I34179c014e455785cd086c671a9ee7b9490c783e

--- services/core/java/com/android/server/wallpaper/WallpaperManagerService.java
+++ services/core/java/com/android/server/wallpaper/WallpaperManagerService.java
@@ -126,6 +126,7 @@ import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
