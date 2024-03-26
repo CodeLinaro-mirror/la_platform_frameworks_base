22676233010b2bcf8e41ddafc0f8003e534aeb55
Dmitry Dementyev <dementyev@google.com>
2024-03-26 10:31:44 -0700

    Add more checkKeyIntent checks to AccountManagerService.
    Another verification is needed after Bundle modification.
    Bug: 321941232
    Test: manual
    
    Change-Id: I9e45d758a2320328da5664b6341eafe6f285f297
    (cherry picked from commit 36db8a1d61a881f89fdd3911886adcda6e1f0d7f)
    (cherry picked from commit 9dd301d63a9228646e01e31a737d32961448b2e4)
    (cherry picked from commit 2d98552d9f14aa9d71738054ffc5886176357159)

--- services/core/java/com/android/server/accounts/AccountManagerService.java
+++ services/core/java/com/android/server/accounts/AccountManagerService.java
@@ -3557,6 +3557,11 @@ public class AccountManagerService
 
             // Strip auth token from result.
             result.remove(AccountManager.KEY_AUTHTOKEN);
+            if (!checkKeyIntent(Binder.getCallingUid(), result)) {
+                onError(AccountManager.ERROR_CODE_INVALID_RESPONSE,
+                        "invalid intent in bundle returned");
+                return;
+            }
 
             if (Log.isLoggable(TAG, Log.VERBOSE)) {
                 Log.v(TAG,
@@ -5125,6 +5130,11 @@ public class AccountManagerService
                     } else {
                         if (mStripAuthTokenFromResult) {
                             result.remove(AccountManager.KEY_AUTHTOKEN);
+                            if (!checkKeyIntent(Binder.getCallingUid(), result)) {
+                                onError(AccountManager.ERROR_CODE_INVALID_RESPONSE,
+                                        "invalid intent in bundle returned");
+                                return;
+                            }
                         }
                         if (Log.isLoggable(TAG, Log.VERBOSE)) {
                             Log.v(TAG, getClass().getSimpleName()
