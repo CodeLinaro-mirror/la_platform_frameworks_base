4c39c9da3cd5b272831a11ca171f1cd4349a4b1c
Sinduran Sivarajan <sinduran@google.com>
2025-02-04 01:59:53 -0800

    Disable "Developer options" by default for managed profiles.
    Bug: 382064697
    Test: go/work-profile-creation-developer-access
    Flag: EXEMPT bugfix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:73b54cdf4b70831c4f952d7556274609cb46214e)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:3298a1c9eb899d02e532f5dcf88904640a626dcd)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:658e0650316574fa729e626c5c4dc2741b28cc09)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ac60be9cff847d06f9bf3a6de6d604347f2fbfea)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:d0d7e44264d36199daaa580c074509bdffbc2b55)
    Merged-In: Ibe6b721f2552d9e72aba0582a2eed4ba87178c7c
    Change-Id: Ibe6b721f2552d9e72aba0582a2eed4ba87178c7c
    (cherry picked from commit 2c2e2cda660fa2da1d9956215ca1550df57ce8ea)
    (cherry picked from commit 3e760f77d2738dd4fea6f5313508676d490427a7)

--- services/tests/servicestests/src/com/android/server/devicepolicy/DevicePolicyManagerServiceMigrationTest.java
+++ services/tests/servicestests/src/com/android/server/devicepolicy/DevicePolicyManagerServiceMigrationTest.java
@@ -359,6 +359,7 @@ public class DevicePolicyManagerServiceMigrationTest extends DpmTestBase {
         // Check that default restrictions were applied.
         DpmTestUtils.assertRestrictions(
                 DpmTestUtils.newRestrictions(
+                        UserManager.DISALLOW_DEBUGGING_FEATURES,
                         UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES,
                         UserManager.DISALLOW_BLUETOOTH_SHARING
                 ),
@@ -366,8 +367,9 @@ public class DevicePolicyManagerServiceMigrationTest extends DpmTestBase {
 
         final Set<String> alreadySet =
                 dpms.getProfileOwnerAdminLocked(10).defaultEnabledRestrictionsAlreadySet;
-        assertThat(alreadySet).hasSize(1);
+        assertThat(alreadySet).hasSize(2);
         assertThat(alreadySet.contains(UserManager.DISALLOW_BLUETOOTH_SHARING)).isTrue();
+        assertThat(alreadySet.contains(UserManager.DISALLOW_DEBUGGING_FEATURES)).isTrue();
     }
 
     @SmallTest
