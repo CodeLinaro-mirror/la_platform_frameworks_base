OrionOS-Project/frameworks_base

Revise the checking logic of checkPackage
This method used to do a quick check to validate if a package name
belongs to a UID. However, it also returns positive when the given
package isn't existed.

Test: atest AppOpsTests CtsAppOpsTestCases AppEnumerationTests
Bug: 203430648
Change-Id: I5c4702df73829178775862cce8fbd43f2c3a3d8f

services/core/java/com/android/server/appop/AppOpsService.java
@@ -3307,13 +3307,21 @@ public class AppOpsService extends IAppOpsService.Stub {
         Objects.requireNonNull(packageName);
         try {
             verifyAndGetBypass(uid, packageName, null);
-            if (filterAppAccessUnlocked(packageName)) {
-                return AppOpsManager.MODE_ERRORED;
+            // When the caller is the system, it's possible that the packageName is the special
+            // one (e.g., "root") which isn't actually existed.
+            if (resolveUid(packageName) == uid
+                    || (isPackageExisted(packageName) && !filterAppAccessUnlocked(packageName))) {
+                return AppOpsManager.MODE_ALLOWED;
             }
-            return AppOpsManager.MODE_ALLOWED;
+            return AppOpsManager.MODE_ERRORED;
         } catch (SecurityException ignored) {
             return AppOpsManager.MODE_ERRORED;
         }
+    }
+
+    private boolean isPackageExisted(String packageName) {
+        return LocalServices.getService(PackageManagerInternal.class)
+                .getPackageSetting(packageName) != null;
     }
 
     /**

CodeLinaro-mirror/la_platform_frameworks_base

(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:66fb9ef96092371a47ebac98c3c673f91aaf39cf)
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ad93d9c67c300ffcd8dd4755293b0fbafe78b6ba)
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:1954abf3a3a58f1f64587366c780abe3b81b99c2)
Merged-In: I5c4702df73829178775862cce8fbd43f2c3a3d8f
(cherry picked from commit 18abbc3850e0eda7577b9715dce8cec386783fcd)

services/core/java/com/android/server/appop/AppOpsService.java
@@ -3308,13 +3308,21 @@ public class AppOpsService extends IAppOpsService.Stub {
         Objects.requireNonNull(packageName);
         try {
             verifyAndGetBypass(uid, packageName, null);
-            if (filterAppAccessUnlocked(packageName)) {
-                return AppOpsManager.MODE_ERRORED;
+            // When the caller is the system, it's possible that the packageName is the special
+            // one (e.g., "root") which isn't actually existed.
+            if (resolveNonAppUid(packageName) == uid
+                    || (isPackageExisted(packageName) && !filterAppAccessUnlocked(packageName))) {
+                return AppOpsManager.MODE_ALLOWED;
             }
-            return AppOpsManager.MODE_ALLOWED;
+            return AppOpsManager.MODE_ERRORED;
         } catch (SecurityException ignored) {
             return AppOpsManager.MODE_ERRORED;
         }
+    }
+
+    private boolean isPackageExisted(String packageName) {
+        return LocalServices.getService(PackageManagerInternal.class)
+                .getPackageSetting(packageName) != null;
     }
 
     /**